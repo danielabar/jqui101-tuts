dce54c717034858cf6db8731583da4d5e8417629
Daniela Baron <baron.daniela@gmail.com>
2014-07-19 14:16:04 -0400

    Deployed by grunt gh-pages

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,6 +2,38 @@
 
 (function() {
 
+  // Default
   $('#slider1').slider();
+
+  // Vertical
+  $('#slider2').slider({
+    orientation: 'vertical'
+  });
+
+  // Value
+  $('#slider3').slider({
+    'value': 50
+  });
+
+  // Step
+  $('#slider4').slider({
+    'step': 10
+  });
+
+  // Values
+  $('#slider5').slider({
+    'values': [25, 75]
+  });
+
+  // Value Method
+  $('#slider6').slider();
+  $('#getValue').on('click', function() {
+    var currentValue = $('#slider6').slider('value');
+    toastr.info('Value Method Slider is: ' + currentValue);
+  });
+  $('#setValue').on('click', function() {
+    $('#slider6').slider('value', 99);
+    toastr.info('Value Method Slider changed to: ' + 99);
+  });
 
 })();
